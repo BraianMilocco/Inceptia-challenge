d44807b10d140963cd5f0501cbef74e53c6b074b
Braian <braian.cnlp@gmail.com>
2023-07-05 17:26:48 -0300

    move singleton logic to decorator

--- ej22.py
+++ ej22.py
@@ -25,48 +25,54 @@ class AttemptsManager(metaclass=Singleton):
     def reset_attempts(self):
         self.attempts = 0
 
-    def have_attempts(self):
+    def has_attempts(self):
         return self.attempts < self.max_attempts
 
+def validate_attempts(func):
+    """Decorador para validar la cantidad de intentos de un usuario"""
+    def wrapper(*args, **kwargs):
+        attempts_manager = AttemptsManager()
+        if not attempts_manager.has_attempts():
+            raise Exception("No tiene más intentos")
+        attempts_manager.add_attempt()
+        result = func(*args, **kwargs)
+        if result:
+            attempts_manager.reset_attempts()
+        return result
+    return wrapper
 
 _PRODUCT_DF = pd.DataFrame({
     "product_name": ["Chocolate", "Granizado", "Limon", "Dulce de Leche"],
     "quantity":[3,10,0,5]
 })
 
-
+@validate_attempts
 def is_product_available(product_name, quantity):
     """Devuelve True si el producto esta disponible en la cantidad solicitada
-    Se agregan los intentos a un singleton para poder controlar la cantidad de intentos de un usuario
-    sin cambiara los parametros de la funcion, en caso de que no haya mas intentos se lanza una excepcion
-    para terminar con un posible loop infinito"""
-    attempts_manager = AttemptsManager()
-    if not attempts_manager.have_attempts():
-        raise Exception("No tiene mas intentos")
-    attempts_manager.add_attempt()
+    Se agregar un decorador para validar la cantidad de intentos del usuario
+    y evitar un posible loop infinito, sin modificar la logica de la funcion"""
     if quantity < 1:
         return False
     df = _PRODUCT_DF.loc[_PRODUCT_DF['product_name'].str.upper() == product_name.upper()]
     if df.empty or df['quantity'].iloc[0] < quantity:
         return False
-    attempts_manager.reset_attempts()
     return True
 
 
 
 # En caso de que se quiera probar solo este ejercicio, descomentar las siguientes lineas
 
-# if __name__ == "__main__":
-#     product_name = input("Ingrese el nombre del producto: ")
-#     quantity = int(input("Ingrese la cantidad del producto: "))
-#     try:
-#         available = is_product_available(product_name, quantity)
-#         while not available:
-#             print("El producto no esta disponible, pruebe nuevamente")
-#             product_name = input("Ingrese el nombre del producto: ")
-#             quantity = int(input("Ingrese la cantidad del producto: "))
-#             available = is_product_available(product_name, quantity)
-#         print("El producto esta disponible")
-#     except Exception as e:
-#         print("No tiene mas intentos")
-#         exit()
+if __name__ == "__main__":
+    product_name = input("Ingrese el nombre del producto: ")
+    quantity = int(input("Ingrese la cantidad del producto: "))
+    try:
+        available = is_product_available(product_name, quantity)
+        while not available:
+            print("El producto no esta disponible, pruebe nuevamente")
+            product_name = input("Ingrese el nombre del producto: ")
+            quantity = int(input("Ingrese la cantidad del producto: "))
+            available = is_product_available(product_name, quantity)
+        print("El producto esta disponible")
+    except Exception as e:
+        print("No tiene mas intentos")
+        exit()
